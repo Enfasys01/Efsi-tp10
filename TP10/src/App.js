9925325d3ff84f4d8605f47aed9d1fca5233aac1
Matiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <102532863+MatiMCode@users.noreply.github.com>
2022-10-03 10:27:41 -0300

    gfkls kdjfbnkd

--- TP10/src/App.js
+++ TP10/src/App.js
@@ -12,22 +12,25 @@ import Home from './Routes/Home';
 import QuienesSomos from './Routes/QuienesSomos';
 import Productos from './Routes/Productos';
 import Contacto from './Routes/Contacto';
+import { CartProvider } from './Context/CartContext';
 
 
 
 function App() {
   return (
     <>
+        <CartProvider value={''}>
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout/>}>
-          <Route index path='/Home' element={<Home/>}/>
+          <Route index path='/' element={<Home/>}/>
           <Route path='/QuienesSomos' element={<QuienesSomos/>}/>
           <Route path='/Productos' element={<Productos/>}/>
           <Route path='/Contacto' element={<Contacto/>}/>
         </Route>
       </Routes>
       </BrowserRouter>
+        </CartProvider>
     </>
   );
 }
--- TP10/src/Components/Navbar.js
+++ TP10/src/Components/Navbar.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import {AiOutlineShoppingCart} from 'react-icons/ai'
 const options=[
-  {title:'Home', path:'Home'},
-  {title:'Quienes somos', path:'QuienesSomos'},
-  {title:'Productos', path:'Productos'},
-  {title:'Contacto', path:'Contacto'},
+  {title:'Home', path:'/'},
+  {title:'Quienes somos', path:'/QuienesSomos'},
+  {title:'Productos', path:'/Productos'},
+  {title:'Contacto', path:'/Contacto'},
 ]
 
 export default function Navbar(){
@@ -19,10 +20,15 @@ export default function Navbar(){
               <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav mb-2 mb-lg-0 text-uppercase">
                   {options.map((e,index)=>{return(<>
-                  <li class="nav-item" key={index}>
+                    <li class="nav-item" key={index}>
                     <div class="nav-link" ><Link to={e.path}>{e.title}</Link></div>
                   </li>
                   </>)})}
+                  <li class="nav-item " >
+                    <div className="nav-link">
+                      <AiOutlineShoppingCart size={30}/>
+                    </div>
+                  </li>
                 </ul>
               </div>
             </div>
